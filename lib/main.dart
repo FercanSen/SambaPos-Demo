282101b46a2eb85c910e684cae2cde2dd52bc966
fercansen <fercansen@gmail.com>
2021-07-27 19:15:06 +0300

    Can display image from yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import "package:flutter/services.dart" show rootBundle;
+
+import "package:yaml/yaml.dart";
 
 void main() {
   runApp(MyApp());
@@ -24,13 +27,44 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Map items = {};
+
+  Future getYaml() async {
+    final yamlString =
+        await rootBundle.loadString("assets/yaml_files/menu.yaml");
+    final yamlMap = loadYaml(yamlString);
+    setState(() {
+      items = yamlMap;
+    });
+  }
+
+  @override
+  void initState() {
+    getYaml();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("SambaPOS"),
       ),
-      body: Container(),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print("Pressed Button");
+                print(items["menus"][0]["items"][0]["image"]);
+              },
+              child: Text("Print"),
+            ),
+            Text(items["menus"][0]["items"][0]["image"]),
+            Image.asset(items["menus"][0]["items"][0]["image"]),
+          ],
+        ),
+      ),
     );
   }
 }
