2490871c7cc4907984f6d7af1ae0b67eb771ed96
fercansen <fercansen@gmail.com>
2021-07-29 16:49:16 +0300

    null check added, gesture detector removed

--- lib/widgets/submenu_item.dart
+++ lib/widgets/submenu_item.dart
@@ -8,38 +8,40 @@ class SubMenuItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: GridTile(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 3,
-              )
-            ],
-          ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset(
-              itemList[index]["image"],
-              fit: BoxFit.cover,
-            ),
+    return GridTile(
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 1,
+              blurRadius: 3,
+            )
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            itemList[index]["image"],
+            fit: BoxFit.cover,
           ),
         ),
-        footer: ClipRRect(
-          borderRadius: BorderRadius.circular(10),
-          child: GridTileBar(
-            title: Text(itemList[index]["name"]),
-            trailing: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 4.0),
-              child: Text(
-                itemList[index]["price"].toString(),
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
+      ),
+      footer: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTileBar(
+          title: itemList[index]["name"] != null
+              ? Text(itemList[index]["name"])
+              : Text(itemList[index]["caption"]),
+          trailing: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: (itemList[index]["price"] != null)
+                ? Text(
+                    itemList[index]["price"].toString(),
+                    style: TextStyle(color: Colors.white),
+                  )
+                : Text(""),
           ),
         ),
       ),
