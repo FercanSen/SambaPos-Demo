bbeb8deda3aec4d6d43655463411e008dc81a2d9
fercansen <fercansen@gmail.com>
2021-07-29 16:54:48 +0300

    Drink Screen

---
+++ lib/screens/submenus.dart/drink_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:sambapos_demo/providers/utility_provider.dart';
+import 'package:sambapos_demo/screens/submenus.dart/dessert_screen.dart';
+import 'package:sambapos_demo/widgets/submenu_item.dart';
+
+class DrinksPage extends StatelessWidget {
+  static const routeName = "/drink";
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<UtilityProvider>(context, listen: false);
+    final List subMenu = ModalRoute.of(context)?.settings.arguments as List;
+    final List sideDishes = provider.items["menus"][10]["items"];
+    final subMenuItems = provider.items["menus"][10];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("İçecek Menüsü"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  subMenuItems["description"],
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(10),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 3 / 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                  ),
+                  shrinkWrap: true,
+                  itemCount: sideDishes.length,
+                  itemBuilder: (context, index) => GestureDetector(
+                    onTap: () {
+                      if (subMenu.contains("indirimli-menu-tatli")) {
+                        Navigator.of(context).pushNamed(
+                          DessertPage.routeName,
+                          arguments: subMenu,
+                        );
+                      }
+                    },
+                    child: SubMenuItem(index, sideDishes),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
